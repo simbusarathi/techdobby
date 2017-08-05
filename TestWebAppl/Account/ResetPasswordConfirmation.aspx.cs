6628538927cac5da7f3f8848c3252d620255833b
user <user@SILAMBARASAN>
2017-08-06 02:46:10 +0530

    Add project files.

---
+++ TestWebAppl/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestWebAppl.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TestWebAppl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestWebAppl.Global" Language="C#" %>
